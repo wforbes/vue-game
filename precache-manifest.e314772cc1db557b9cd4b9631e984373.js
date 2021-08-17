b276794ce67b7568ff274b70e45df54635c5dedf
Will Forbes <wforbes87@gmail.com>
2021-08-17 01:07:26 -0700

    Still trying to add day2-2 dist

---
+++ precache-manifest.e314772cc1db557b9cd4b9631e984373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd8dfe5e5efa281d97a",
+    "url": "/vue-game/css/app.d7e62148.css"
+  },
+  {
+    "revision": "40c6f568aa2151e1eede299c4127fcef",
+    "url": "/vue-game/index.html"
+  },
+  {
+    "revision": "8bd8dfe5e5efa281d97a",
+    "url": "/vue-game/js/app.80b1bf62.js"
+  },
+  {
+    "revision": "a49cfc5bcd8ceefa57fb",
+    "url": "/vue-game/js/chunk-vendors.40979ff8.js"
+  },
+  {
+    "revision": "0bcf03303b18d7c0782b500cab8ae592",
+    "url": "/vue-game/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-game/robots.txt"
+  }
+]);
